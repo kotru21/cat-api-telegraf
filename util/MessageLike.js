c7333242df13baeaa7021911b2f265cd0363892b
Arsenij Kotikov <88907641+kotru21@users.noreply.github.com>
2025-01-10 14:47:53 +0300

    refactor logic

--- util/MessageLike.js
+++ util/MessageLike.js
@@ -2,16 +2,28 @@ import AddLikes from "../util/addLikes.js";
 import GetLikes from "../util/getLikes.js";
 import { Composer, Markup } from "telegraf";
 
-export default Composer.action(/^data-(.*?)$/, (ctx) => {
-  GetLikes(ctx.match[1]).then(
-    (
-      LikesCount //console.log(LikesCount[0].count)
-    ) =>
-      ctx.editMessageReplyMarkup({
-        inline_keyboard: [[Markup.button.callback(`👍 ${LikesCount[0].count + 1}`, `data`)]],
-      }),
-    AddLikes(ctx.match[1]).then(() => {
-      return ctx.answerCbQuery(`Лайкнуто, id: ${ctx.match[1]}! 👍`);
-    })
-  );
+export default Composer.action(/^data-(.*?)$/, async (ctx) => {
+  try {
+    const catId = ctx.match[1];
+
+    // add like
+    await AddLikes(catId);
+
+    // Get updated likes count
+    const likes = await GetLikes(catId);
+    const likesCount = likes[0].count;
+
+    // Update the message with the new likes count
+    await ctx.editMessageReplyMarkup({
+      inline_keyboard: [
+        [Markup.button.callback(`👍 ${likesCount}`, `data-${catId}`)],
+      ],
+    });
+
+    // Send a notification to the user
+    await ctx.answerCbQuery(`Лайкнуто! 👍`);
+  } catch (error) {
+    console.error("Ошибка при обработке лайка:", error);
+    await ctx.answerCbQuery("Произошла ошибка при обработке лайка");
+  }
 });

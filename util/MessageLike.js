da9ba3ed213b8001a6744ddc230989be2c098a44
Arsenij Kotikov <88907641+kotru21@users.noreply.github.com>
2025-01-11 23:29:49 +0300

    heroku

--- util/MessageLike.js
+++ util/MessageLike.js
@@ -1,5 +1,5 @@
 import AddLikes from "./addlikes.js";
-import GetLikes from "./getlikes.js";
+import GetLikes from "./etLikes.js";
 import { Composer, Markup } from "telegraf";
 
 export default Composer.action(/^data-(.*?)$/, async (ctx) => {

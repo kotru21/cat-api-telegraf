29294bcb0f9037647e7117b66051944a00fc7d68
Arsenij Kotikov <88907641+kotru21@users.noreply.github.com>
2025-01-12 00:02:03 +0300

    Update server.js

--- util/server.js
+++ util/server.js
@@ -80,7 +80,7 @@ export default function webServer(port) {
     fs.readFile(filePath, "utf8", (err, html) => {
       if (err) return res.status(500).send("Internal Server Error");
       const modified = html
-        .replace("{{websocketPort}}", port)
+        .replace("{{websocketUrl}}", `wss://${req.headers.host}/ws`)
         .replace("{{apiPort}}", port);
       res.send(modified);
     });
